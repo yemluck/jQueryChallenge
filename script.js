db11f4c119d17fb75f23a513e5c2db78eeb4dbe8
Chris Agbolade <yemluck@gmail.com>
2021-12-09 11:59:52 -0600

    grabbed first h1 and changed text

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ function onReady(){
 
     console.log('in onReady')
 
+    $('h1:first').text('jQuery challenge 1')
+    $('h2').parent().css('background-color', 'yellow')
+    $('h2').parent().children().css('background-color',"green")
+
+
 
 } // end function onReady
